63a64441b64e8f32f84518048891529e578bc504
Eden Le <edenlee@Edens-MacBook-Pro.local>
2021-04-20 12:54:33 +0700

    landing page

--- src/views/LandingPage/Sections/ProductSection.js
+++ src/views/LandingPage/Sections/ProductSection.js
@@ -48,6 +48,7 @@ const useStyles = makeStyles(theme => ({
     marginTop: '100px'
   },
   serviceRaised: {
+    margin: "0px 16px",
     borderRadius: '24px',
     boxShadow:
       '0 16px 24px 2px rgba(0, 0, 0, 0.14), 0 6px 30px 5px rgba(0, 0, 0, 0.12), 0 8px 10px -5px rgba(0, 0, 0, 0.2)'
